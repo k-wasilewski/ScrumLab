9850ece58facbe5707137e8f97384528bc9eb843
k-wasilewski <k.k.wasilewski@gmail.com>
2019-12-16 08:55:56 +0100

    Recipe & RecipeDAO

---
+++ src/main/java/pl/coderslab/model/Recipe.java
@@ -0,0 +1,96 @@
+package pl.coderslab.model;
+
+import java.util.Date;
+
+public class Recipe {
+    private int id;
+    private Date created;
+    private Date updated;
+    private String name;
+    private String ingredients;
+    private String description;
+    private int preparation_time;
+    private String preparation;
+    private int admin_id;
+
+    public Recipe() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getIngredients() {
+        return ingredients;
+    }
+
+    public void setIngredients(String ingredients) {
+        this.ingredients = ingredients;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getPreparation_time() {
+        return preparation_time;
+    }
+
+    public void setPreparation_time(int preparation_time) {
+        this.preparation_time = preparation_time;
+    }
+
+    public String getPreparation() {
+        return preparation;
+    }
+
+    public void setPreparation(String preparation) {
+        this.preparation = preparation;
+    }
+
+    public int getAdmin_id() {
+        return admin_id;
+    }
+
+    public void setAdmin_id(int admin_id) {
+        this.admin_id = admin_id;
+    }
+
+    public Date getCreated() {
+        return created;
+    }
+
+    public void setCreated(Date created) {
+        this.created = created;
+    }
+
+    public Date getUpdated() {
+        return updated;
+    }
+
+    public void setUpdated(Date updated) {
+        this.updated = updated;
+    }
+
+    /*
+    @Override
+    public String toString() {return "Recipe id="+id+", created: "+created.getTime()+", updated: "+updated.getTime()+
+    ", name: "+name+", ingredients: "+ingredients+", description: "+description+", preparation time: "+preparation_time+"," +
+            "admin id: "+admin_id;}*/
+}
