7b464e1bf114cd7f6ee7f5f4330e4c0e797f971f
Jakub Wziątka <kuba.wziatka@gmail.com>
2019-12-16 10:15:10 +0100

    DayNameDao - niepelny CRUD

--- src/main/java/pl/coderslab/utils/DbUtil.java
+++ src/main/java/pl/coderslab/utils/DbUtil.java
@@ -5,6 +5,7 @@ import javax.naming.InitialContext;
 import javax.naming.NamingException;
 import javax.sql.DataSource;
 import java.sql.Connection;
+import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DbUtil {
@@ -25,4 +26,12 @@ public class DbUtil {
         }
         return dataSource;
     }
+
+//    private static final String DB_URL = "jdbc:mysql://localhost:3306/scrumlab?useSSL=false&characterEncoding=utf8";
+//    private static final String DB_USER = "root";
+//    private static final String DB_PASS = "coderslab";
+//
+//    public static Connection getConnection() throws SQLException {
+//        return DriverManager.getConnection(DB_URL, DB_USER, DB_PASS);
+//    }
 }
