1a4be508090eff9e9d8cf21e673cb1288a92c930
k-wasilewski <k.k.wasilewski@gmail.com>
2019-12-17 09:46:04 +0100

    #6_Widok_oraz_servlet_wyświetlające_listę_przepisów

---
+++ src/main/java/pl/coderslab/web/AppRecipesServlet.java
@@ -0,0 +1,29 @@
+package pl.coderslab.web;
+
+import pl.coderslab.dao.RecipeDao;
+import pl.coderslab.model.Recipe;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+import java.util.List;
+
+@WebServlet("/app/recipe/list/")
+public class AppRecipesServlet extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        RecipeDao rdao = new RecipeDao();
+        List<Recipe> recipeList = rdao.findAllDesc();
+        HttpSession sess = request.getSession();
+        sess.setMaxInactiveInterval(3600);
+        sess.setAttribute("recipeList", recipeList);
+        getServletContext().getRequestDispatcher("/app-recipes.jsp").forward(request, response);
+    }
+}
