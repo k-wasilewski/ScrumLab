dbbda393dd07ed5953015df86148f4ab9cb533e5
k-wasilewski <57446233+k-wasilewski@users.noreply.github.com>
2019-12-17 10:14:17 +0100

    updated adminId

--- src/main/java/pl/coderslab/web/AddRecipeServlet.java
+++ src/main/java/pl/coderslab/web/AddRecipeServlet.java
@@ -13,6 +13,9 @@ import java.io.IOException;
 @WebServlet("/app/recipe/add")
 public class AddRecipeServlet extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        HttpSession sess = request.getSession();
+        sess.setMaxInactiveInterval(3600);
+        Admin admin = (Admin) sess.getAttribute("admin");
         String name=request.getParameter("name");
         String description=request.getParameter("description");
         int preparation_time;
@@ -25,7 +28,7 @@ public class AddRecipeServlet extends HttpServlet {
         String ingredients=request.getParameter("ingredients");
 
         Recipe newRecipe = new Recipe();
-        newRecipe.setAdmin_id(1);   //waiting for login handling
+        newRecipe.setAdmin_id(admin.getId());  
         newRecipe.setName(name);
         newRecipe.setDescription(description);
         newRecipe.setPreparation_time(preparation_time);
