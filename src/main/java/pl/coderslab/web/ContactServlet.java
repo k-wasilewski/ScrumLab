704814975b390369f4814b82178295d5525c272a
k-wasilewski <k.k.wasilewski@gmail.com>
2019-12-19 16:04:24 +0100

    fefe

--- src/main/java/pl/coderslab/web/ContactServlet.java
+++ src/main/java/pl/coderslab/web/ContactServlet.java
@@ -1,11 +1,15 @@
 package pl.coderslab.web;
 
+import pl.coderslab.dao.SuperAdminDao;
+import pl.coderslab.model.Admin;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.List;
 
 @WebServlet("/contact")
 public class ContactServlet extends HttpServlet {
@@ -14,6 +18,9 @@ public class ContactServlet extends HttpServlet {
     }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        SuperAdminDao sadao = new SuperAdminDao();
+        List<Admin> superadmins = sadao.findAllSuperadmins();
+        request.setAttribute("superadmins", superadmins);
         getServletContext().getRequestDispatcher("/contact.jsp").forward(request, response);
     }
 }
